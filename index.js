a5157ed1d0a6b044f56895c7b139fca866c652f8
davey <davey89451@gmail.com>
2021-06-11 10:31:35 -0700

    True Count and Bet Increments Added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let currentArr = [];
+const deckCount = document.getElementById('deckNumber')
 
 function cardArray(card) {
     currentArr.push(card);
@@ -6,18 +7,35 @@ function cardArray(card) {
     countCards(currentArr);
 }
 
-function countCards(arr){
+function countCards(arr, decks){
     let count = 0;
+    decks = deckCount.value * 52;
+
     for (var i = 0; i < arr.length; i++){
         let cards = arr[i];
 
        if(cards === 'a' || cards === 'k' || cards === 'q' || cards === 'j' || cards === '10'){
-           count -= 1
+           count -= 1;
+           decks -= 1;
        }
         else if(cards === '2' || cards === '3' || cards == '4' || cards === '5'|| cards === '6' ){
-            count++
+            count++;
+            decks--
         }
-       document.getElementById('count').innerHTML = count;
+        else{
+            decks--
+        }
+        console.log(decks)
+        let wholeDecks = (decks / 52);
+        let accurateDecks;
+        wholeDecks > 1 ? accurateDecks= Math.round(wholeDecks * 2) / 2 : accurateDecks = 1;
+        let trueCount = Math.ceil(count / accurateDecks)
+        let bettingUnits;
+        trueCount > 2 ? bettingUnits = trueCount - 1 : bettingUnits = 1 ; 
+       document.getElementById('count').innerHTML = "Running Count:" + count;
+       document.getElementById('cardsLeft').innerHTML = "Decks Left" + accurateDecks;
+       document.getElementById('trueCount').innerHTML = "True Count" + trueCount;
+       document.getElementById('bettingUnits').innerHTML= `Bet Size: ${bettingUnits} Units`
     }
 }
 
@@ -25,6 +43,12 @@ function countCards(arr){
 
 let resetArr = () => {
     currentArr = []
-    document.getElementById('count').innerHTML = currentArr
+    document.getElementById('count').innerHTML = "Running Count:"
+    document.getElementById('cardsLeft').innerHTML = "Decks Left:"
+    document.getElementById('trueCount').innerHTML = "True Count:"
+    document.getElementById('bettingUnits').innerHTML= "BetSize:"
 }
 
+function getValue(){
+console.log(deckCount.value)
+}
